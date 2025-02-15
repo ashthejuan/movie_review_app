e21807b8b93ad35b3c3fb6bcbe1670d085a17057
ashthejuan <akshitdg.7564@gmail.com>
2025-02-15 11:04:21 +0530

    Added the code for the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,191 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MovieReviewApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
+class MovieReviewApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Movie Review App',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primaryColor: Color(0xFF171738),
+        scaffoldBackgroundColor: Color(0xFFC9CAD9),
+
+        colorScheme: ColorScheme.light(
+          primary: Color(0xFF171738),
+          onPrimary: Colors.white,
+        ),
+        textTheme: TextTheme(
+          titleLarge:TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Color(0xFF171738)),
+          bodyMedium: TextStyle(fontSize: 16, color: Color(0xFF171738)),
+          bodySmall: TextStyle(fontSize: 14, color: Color(0xFF171738)),
+        ),
+
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Color(0xFF171738),
+            foregroundColor: Colors.white,
+          ),
+        ),
+
+        inputDecorationTheme: InputDecorationTheme(
+          labelStyle: TextStyle(color: Color(0xFF171738)),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Color(0xFF171738)),
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Color(0xFF171738)),
+          ),
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MovieReviewScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class MovieReviewScreen extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _MovieReviewScreenState createState() => _MovieReviewScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _MovieReviewScreenState extends State<MovieReviewScreen> {
+  List<Map<String, String>> reviews = [];
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+
+  final TextEditingController _movieNameController = TextEditingController();
+  final TextEditingController _genreController = TextEditingController();
+  final TextEditingController _reviewContoller = TextEditingController();
+
+  void _addReview(){
+    if (_movieNameController.text.isNotEmpty && 
+        _genreController.text.isNotEmpty &&
+        _reviewContoller.text.isNotEmpty){
+          setState(() {
+            reviews.add({
+              'movieName': _movieNameController.text,
+              'genre': _genreController.text,
+              'review': _reviewContoller.text,
+            });
+          });
+
+          _movieNameController.clear();
+          _genreController.clear();
+          _reviewContoller.clear();
+
+          Navigator.of(context).pop();
+        } else {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text('Please fill all fields')),
+          );
+        }
   }
 
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+  void _showAddReviewDialog(){
+    showDialog(context: context, builder: (context){
+      return AlertDialog(
+        title: Text('Add Review'),
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            TextField(
+              controller: _movieNameController,
+              decoration: InputDecoration(labelText: 'Movie Name'),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            TextField(
+              controller: _genreController,
+              decoration: InputDecoration(labelText: 'Genre'),
+            ),
+            TextField(
+              controller: _reviewContoller,
+              maxLines: 3,
+              maxLength: 180,
+              decoration: InputDecoration(labelText: 'Review (180 words max)'),
             ),
           ],
         ),
+        actions: [
+          TextButton(onPressed: (){
+            Navigator.of(context).pop();
+          },
+          child: Text('Cancel'),
+          ),
+          ElevatedButton(onPressed: _addReview, child: Text('Add')),
+        ],
+      );
+    },
+    );
+  }
+
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Movie Review App'),
+        backgroundColor: Color(0xFF171738),
       ),
+      body: reviews.isEmpty ? Center(
+        child: Text(
+          'No reviews yet. Tap "+" to add one.',
+          style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Color(0xFF171738)),
+        ),
+      )
+    : ListView.builder(
+      itemCount: reviews.length,
+      itemBuilder: (context, index) {
+        final review = reviews[index];
+        return Container(
+          padding: EdgeInsets.all(16),
+          margin: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(8),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black12,
+                blurRadius: 4,
+                offset: Offset(0, 2),
+              ),
+            ],
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                review['movieName']!,
+                style: Theme.of(context).textTheme.titleLarge,
+              ),
+              SizedBox(height: 8),
+              Text(
+                'Genre: ${review['genre']}',
+                style: Theme.of(context).textTheme.bodyMedium,
+              ),
+              SizedBox(height: 8),
+              Text(
+                review['review']!,
+                style: Theme.of(context).textTheme.bodySmall,
+              ),
+            ],
+          ),
+        );
+      },
+    ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        onPressed: _showAddReviewDialog,
+        backgroundColor: Color(0xFF171738),
+        child: const Icon(Icons.add, color: Colors.white),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Text(
+          'Add Review',
+          textAlign: TextAlign.center,
+          style: TextStyle(fontSize: 16, color: Color(0xFF171738)),
+        ),
+      ),
     );
   }
 }
